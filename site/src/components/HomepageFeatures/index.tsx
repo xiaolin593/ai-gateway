39c969a82dd499f663411258179fc5d188716d86
Dennis Urban <mail@dpunkturban.io>
2026-02-14 20:08:58 +0100

    chore(website): update frontpage version from 0.4 to 0.5 (#1861)

--- site/src/components/HomepageFeatures/index.tsx
+++ site/src/components/HomepageFeatures/index.tsx
@@ -19,11 +19,11 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'v0.4 Release now available',
+    title: 'v0.5 Release now available',
     image: require('@site/static/img/3.png').default,
     description: (
       <>
-        The v0.4 Release of Envoy AI Gateway is now available. See the <a href="/release-notes/v0.4">release notes</a> for more information.
+        The v0.5 Release of Envoy AI Gateway is now available. See the <a href="/release-notes/v0.5">release notes</a> for more information.
       </>
     ),
   },
