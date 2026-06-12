c23fc4bc0ae5146c90a926ef4965e0f11e2a6de6
hustxiayang <yxia216@bloomberg.net>
2026-06-12 15:05:43 -0400

    fix: allow strict function definition in anthropic (#2232)

--- internal/translator/anthropic_helper.go
+++ internal/translator/anthropic_helper.go
@@ -140,6 +140,10 @@ func translateOpenAItoAnthropicTools(openAITools []openai.Tool, openAIToolChoice
 				Description: anthropic.String(openAITool.Function.Description),
 			}
 
+			if openAITool.Function.Strict {
+				toolParam.Strict = anthropic.Bool(true)
+			}
+
 			if isCacheEnabled(openAITool.Function.AnthropicContentFields) {
 				toolParam.CacheControl = anthropic.NewCacheControlEphemeralParam()
 			}
