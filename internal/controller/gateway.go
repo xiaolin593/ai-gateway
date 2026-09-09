fa4cb870f14029f863f5035abf8c8301cf2e7621
Xiaolin Lin <xlin158@bloomberg.net>
2026-09-09 17:15:57 -0400

    format
    Signed-off-by: Xiaolin Lin <xlin158@bloomberg.net>

--- internal/controller/gateway.go
+++ internal/controller/gateway.go
@@ -170,11 +170,12 @@ func (c *GatewayController) Reconcile(ctx context.Context, req ctrl.Request) (ct
 func schemaToFilterAPI(schema aigv1b1.VersionedAPISchema) filterapi.VersionedAPISchema {
 	ret := filterapi.VersionedAPISchema{}
 	ret.Name = filterapi.APISchemaName(schema.Name)
-	if schema.Name == aigv1b1.APISchemaOpenAI || schema.Name == aigv1b1.APISchemaAnthropic {
+	switch schema.Name {
+	case aigv1b1.APISchemaOpenAI, aigv1b1.APISchemaAnthropic:
 		ret.Prefix = cmp.Or(ptr.Deref(schema.Prefix, ""), "v1")
-	} else if schema.Name == aigv1b1.APISchemaAWSOpenAI {
+	case aigv1b1.APISchemaAWSOpenAI:
 		ret.Prefix = cmp.Or(ptr.Deref(schema.Prefix, ""), "openai/v1")
-	} else {
+	default:
 		ret.Version = ptr.Deref(schema.Version, "")
 	}
 	return ret
